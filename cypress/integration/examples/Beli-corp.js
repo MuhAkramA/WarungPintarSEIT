1f2d94233f3f86d2e7ad08daa61b7dca818fab45
MuhAkramA <robinvanakram@gmail.com>
2021-04-11 17:51:33 +0700

    edit file

--- cypress/integration/examples/Beli-corp.js
+++ cypress/integration/examples/Beli-corp.js
@@ -11,31 +11,23 @@ describe('My First Test', function() {
     cy.get("button.portfolio-new").contains("PILIH PRODUK").click()
     cy.get('.product-name').should('have.length', 32)
     cy.get('.product-name:visible').should('have.length', 32)
-    // cy.get("#product-list").find("span.product-name").eq(0).click()
-    cy.get("#product-list").find("span.product-name").each(($el, index, $list) => {
-      const textProd = $el.find('span.product-name').text()
-      if (textProd.includes('Batavia')) {
-        $el.find('button').click()
-      }
-    })
-    cy.get(".product-buy").find('button.btn-block').each(($el, index, $list) => {
+    cy.get(".product-buy").each(($el) => {
 
-      const textProd = $el.find('BELI').text()
-      if (textProd.includes('BELI')) {
-        $el.find('button').click()
+    cy.log($el.text()) 
+    cy.wrap($el).should('contain', 'BELI').eq(0).click()
       }
-    })
+    )
 
-    cy.get(".product-buy").eq(0).contains('BELI').click()
-    cy.get('#buy_amount').type("500000")
-    cy.get('button.btn-buy-continue').contains("LANJUT").click()
-    cy.get(".payment-method-div").click()
-    // cy.get("#aggre_div > label > span").click()
-    // cy.get("#prospectus-modal").find("button.close").click()
-    // cy.get("#continue").click()
-    cy.get(".navbar-logo").then(function(logbusiness)
-  {
-    cy.log(logbusiness.text())
+  //   // cy.get(".product-buy").eq(0).contains('BELI').click()
+  //   cy.get('#buy_amount').type("500000")
+  //   cy.get('button.btn-buy-continue').contains("LANJUT").click()
+  //   cy.get(".payment-method-div").click()
+  //   // cy.get("#aggre_div > label > span").click()
+  //   // cy.get("#prospectus-modal").find("button.close").click()
+  //   // cy.get("#continue").click()
+  //   cy.get(".navbar-logo").then(function(logbusiness)
+  // {
+  //   cy.log(logbusiness.text())
   })
   })
-})
+// })
