76217384a2785c80a6daf7efe4debc90472ad51c
MuhAkramA <robinvanakram@gmail.com>
2021-04-11 20:47:16 +0700

    lord

--- cypress/integration/examples/Beli-corp.js
+++ cypress/integration/examples/Beli-corp.js
@@ -11,23 +11,22 @@ describe('My First Test', function() {
     cy.get("button.portfolio-new").contains("PILIH PRODUK").click()
     cy.get('.product-name').should('have.length', 32)
     cy.get('.product-name:visible').should('have.length', 32)
-    cy.get(".product-buy").each(($el) => {
+    // cy.get("div .product-buy").each(($el) => {
+    //   cy.log($el.text()) 
+    //   cy.wrap($el).should('contain', 'BELI').eq(0).click()
+    //   }
+    // )
 
-    cy.log($el.text()) 
-    cy.wrap($el).should('contain', 'BELI').eq(0).click()
-      }
-    )
-
-  //   // cy.get(".product-buy").eq(0).contains('BELI').click()
-  //   cy.get('#buy_amount').type("500000")
-  //   cy.get('button.btn-buy-continue').contains("LANJUT").click()
-  //   cy.get(".payment-method-div").click()
-  //   // cy.get("#aggre_div > label > span").click()
-  //   // cy.get("#prospectus-modal").find("button.close").click()
-  //   // cy.get("#continue").click()
-  //   cy.get(".navbar-logo").then(function(logbusiness)
-  // {
-  //   cy.log(logbusiness.text())
+    cy.get("div .product-buy").eq(1).contains('BELI').click()
+    // cy.get('#buy_amount').type("500000")
+    cy.get('button.btn-buy-continue').contains("LANJUT").click()
+    cy.get(".payment-method-div").click()
+    // cy.get("#aggre_div > label > span").click()
+    // cy.get("#prospectus-modal").find("button.close").click()
+    // cy.get("#continue").click()
+    cy.get(".navbar-logo").then(function(logbusiness)
+  {
+    cy.log(logbusiness.text())
   })
   })
-// })
+})
