0411a141da81e2aef0e04ee480e25d067cf3daad
MuhAkramA <robinvanakram@gmail.com>
2021-04-13 11:51:40 +0700

    status nav

--- cypress/integration/examples/get nav data.js
+++ cypress/integration/examples/get nav data.js
@@ -3,7 +3,20 @@
 // type definitions for custom commands like "createDefaultTodos"
 describe('My First Test', function() {
     it('login', function() {
-        cy
+        cy.request({
+            method: 'GET',
+            url: 'https://hapidev.tanamduit.com/ecommerce/v2.0/mf/product/navhistory/LG002MMSTRKSYA00',
+            headers: {
+                'Accept':'application/json',
+                'Content-Type': 'application/json',
+                'X-Ca-Key':'25887230',
+                'X-Ca-Signature':'IODftEPZmkELU/uHrlf7l/0dNq+kfY3CZhADgMa5bPo=',
+                'Tenant-Id':'25887230',
+            }
+        }).then(response => {
+            expect(response.status).to.eq(201)
+          })
+        
 
     })
 })
