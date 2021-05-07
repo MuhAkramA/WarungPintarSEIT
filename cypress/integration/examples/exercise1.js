147a191220e4dc17d9a44663313a55e00e926991
MuhAkramA <robinvanakram@gmail.com>
2021-05-07 12:38:35 +0700

    new update cypress

---
+++ cypress/integration/examples/exercise1.js
@@ -0,0 +1,13 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+// type definitions for custom commands like "createDefaultTodos"
+describe('My First Test', function() {
+    it('login', function() {
+      cy.visit("https://http://www.ebay.com/")
+      cy.get('')
+
+  
+    })
+    })
+  
